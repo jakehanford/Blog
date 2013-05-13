386964a3d9c7231a8a4fd987ac1f79970aefb6a7
Jack Hanford <jackhanford@gmail.com>
2013-05-13 10:48:45 -0700

    Added some simple validations to authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,5 +2,10 @@ class Author < ActiveRecord::Base
   attr_accessible :email, :name, :password, :twitter, :website
 
   has_many :posts
+  	validates_presence_of :name, :email, :password
+  	validates_length_of :name, maximum: 40
+
+
+
 
 end
